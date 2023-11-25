b6a0b655f5abf30f3aec502e6a75891fddeebb51
Sofia Weng <sowe0001@stud.kea.dk>
2023-11-26 00:45:23 +0100

    laver første udkast

---
+++ script.js
@@ -0,0 +1,13 @@
+//----------------burgermenu---------------
+const hamburger = document.querySelector(".hamburger");
+const navmenu = document.querySelector(".nav-menu");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navmenu.classList.toggle("active");
+});
+document.querySelectorAll(".nav-link").forEach((n) =>
+  n.addEventListener("click", () => {
+    hamburger.classList.remove("active");
+    navmenu.classList.remove("active");
+  })
+);
